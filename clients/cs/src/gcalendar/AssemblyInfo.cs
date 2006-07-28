9a2c26b865a53d585a16f1b67e88b384e825426e
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2006-07-28 15:15:34 +0000

    Added a simple form based Calendar example Fixed a bug in the daterange query creation, that caused a service error if a time zone component was present

--- clients/cs/src/gcalendar/AssemblyInfo.cs
+++ clients/cs/src/gcalendar/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.3.*")]
+[assembly: AssemblyVersion("1.0.4.*")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("calendarkey.sn")]
