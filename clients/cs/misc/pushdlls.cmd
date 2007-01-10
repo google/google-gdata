a894b10c71438f74f556230bb4f18b773714c905
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2007-01-10 13:47:28 +0000

    Added binaries to 1.0.9.0 build. Changed VS2003 project files to use AssemblyVersion.cs, a shared file in src/version. Added the NDOC project file and a .cmd file (cs/misc) to create the build.

---
+++ clients/cs/misc/pushdlls.cmd
@@ -0,0 +1,35 @@
+# build the whole release project, note this should RUN inside the VS dev prompt
+
+echo "Run this inside the Visual Studio Development Prompt"
+devenv /rebuild Release ..\src\VS2003\gdata.sln
+
+
+# copy all exe files
+xcopy /y ..\src\VS2003\Blogger\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\Calendar\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\CodeSearch\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\Spreadsheet\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gbase_customertool\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gbase_demo\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gbase_querytool\bin\Release\*.exe ..\lib\Release\*.*
+
+#copy the DLLS
+xcopy /y ..\src\VS2003\gdata\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gbase\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gextension\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gspreadsheets\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gcalendar\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\VS2003\gcodesearch\bin\Release\*.dll ..\lib\Release\*.*
+
+#now build the documentation
+"%ProgramFiles%\NDoc 1.3\bin\net\1.1\NDocConsole.exe" -project=GDataProject.ndoc
+# the tree.cs generated is buggy, use this one
+copy tree.cs doc\tree.js
+copy doc\Documentation.chm ..\docs\Documentation.chm
+del doc\Documentation.*
+rd -s -q doc\ndoc_msdn_temp
+wzzip HTMLDocumentation.zip doc/*.*
+rd -s -q doc
+copy HTMLDocumentation.zip ..\docs\HTMLDocumentation.zip
+del HTMLDocumentation.zip 
+
