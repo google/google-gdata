3bf3eec5b4b2a4cb68a4a5b4f095e001c580d353
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2007-01-10 11:13:52 +0000

    Changed the mono build to use a single version file, so that one file (version/AssemblyVersion.cs) is responsible for versioning all produced binaries under Mono. VS2003/VS2005 changes follow shortly

--- clients/cs/src/gcodesearch/AssemblyInfo.cs
+++ clients/cs/src/gcodesearch/AssemblyInfo.cs
@@ -15,17 +15,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.8.*")]
-
 [assembly: AssemblyDelaySign(false)]
 #if WindowsCE || PocketPC
 #else
