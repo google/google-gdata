07c758b9f10de5b4a6f0d463a7e323e85a54d7f3
siwuzzz <siwuzzz@b7824a5d-8719-0410-90f2-77ed95a13428>
2007-08-01 19:38:47 +0000

    src/core/gzipstream.cs: - Added XML comments
    src/VS2003/unittests/unittests.csproj:
    - Added GZipStream unit tests
    - Added GZip Live Tests
    
    src/unittests/gziplivetest.cs:
    - Live test using GZip against a calendar feed
    
    src/unittests/gzipstreamtest.cs:
    - GZipStream unit tests

--- clients/cs/src/core/gzipstream.cs
+++ clients/cs/src/core/gzipstream.cs
@@ -4,12 +4,16 @@ using System.Runtime.InteropServices;
 
 namespace Google.GData.Client
 {
+    /// <summary>Type of compression to use for the GZipStream. Currently only Decompress is supported.</summary>
     public enum CompressionMode
 	{
+        /// <summary>Compresses the underlying stream.</summary>
         Compress,
+        /// <summary>Decompresses the underlying stream.</summary>
         Decompress,
 	}
 
+    /// <summary>Provides methods and properties used to compress and decompress streams.</summary>
     public class GZipStream : Stream
     {
         #region Native const, structs, and defs
@@ -129,27 +133,42 @@ namespace Google.GData.Client
         private byte[]              inputBuffer = new byte[BufferSize];
         private GCHandle            inputBufferHandle;
 
+        /// <summary>Initializes a new instance of the GZipStream class using the specified stream and CompressionMode value.</summary>
+        /// <param name="stream">The stream to compress or decompress.</param>
+        /// <param name="mode">One of the CompressionMode values that indicates the action to take.</param>
         public GZipStream(Stream stream, CompressionMode mode)
         {
+            if (mode != CompressionMode.Decompress)
+                throw new NotImplementedException("Compression is not implemented.");
+
             this.compressedStream = stream;
             this.mode = mode;
-
-            this.inputBufferHandle = GCHandle.Alloc(inputBuffer, GCHandleType.Pinned);
 
             this.zstream.zalloc = IntPtr.Zero;
             this.zstream.zfree = IntPtr.Zero;
             this.zstream.opaque = IntPtr.Zero;
 
-            inflateInit2(ref this.zstream, ZLibOpenType.Both, ZLibVersion, Marshal.SizeOf(typeof(z_stream)));
+            ZLibReturnCode ret = inflateInit2(ref this.zstream, ZLibOpenType.Both, ZLibVersion, Marshal.SizeOf(typeof(z_stream)));
+
+            if (ret != ZLibReturnCode.Ok)
+                throw new ArgumentException("Unable to init ZLib. Return code: " + ret.ToString());
+
+            this.inputBufferHandle = GCHandle.Alloc(inputBuffer, GCHandleType.Pinned);
         }
 
+        /// <summary>GZipStream destructor. Cleans all allocated resources.</summary>
         ~GZipStream()
         {
             inputBufferHandle.Free();
             inflateEnd(ref this.zstream);
         }
 
-        public override int Read(byte[] buffer, int offset, int count)
+        /// <summary>Reads a number of decompressed bytes into the specified byte array.</summary>
+        /// <param name="array">The array used to store decompressed bytes.</param>
+        /// <param name="offset">The location in the array to begin reading.</param>
+        /// <param name="count">The number of bytes decompressed.</param>
+        /// <returns>The number of bytes that were decompressed into the byte array. If the end of the stream has been reached, zero or the number of bytes read is returned.</returns>
+        public override int Read(byte[] array, int offset, int count)
         {
             if (this.mode == CompressionMode.Compress)
                 throw new NotSupportedException("Can't read on a compress stream!");
@@ -177,15 +196,15 @@ namespace Google.GData.Client
                     {
                         case ZLibReturnCode.StreamEnd:
                             exitLoop = true;
-                            Array.Copy(tmpOutputBuffer, 0, buffer, offset, count - (int)this.zstream.avail_out);
+                            Array.Copy(tmpOutputBuffer, 0, array, offset, count - (int)this.zstream.avail_out);
                             break;
                         case ZLibReturnCode.Ok:
-                            Array.Copy(tmpOutputBuffer, 0, buffer, offset, count - (int)this.zstream.avail_out);
+                            Array.Copy(tmpOutputBuffer, 0, array, offset, count - (int)this.zstream.avail_out);
                             break;
                         case ZLibReturnCode.MemoryError:
-                            throw new OutOfMemoryException();
+                            throw new OutOfMemoryException("ZLib return code: " + result.ToString());
                         default:
-                            throw new Exception("Zlib Return Code: " + result.ToString());
+                            throw new Exception("ZLib return code: " + result.ToString());
                     }
                 }
 
@@ -197,58 +216,76 @@ namespace Google.GData.Client
             }
         }
 
+        /// <summary>Closes the current stream and releases any resources (such as sockets and file handles) associated with the current stream.</summary>
         public override void Close()
         {
             this.compressedStream.Close();
             base.Close();
         }
 
+        /// <summary>Gets a value indicating whether the stream supports reading while decompressing a file.</summary>
         public override bool CanRead
         {
             get { return (this.mode == CompressionMode.Decompress ? true : false); }
         }
 
+        /// <summary>Gets a value indicating whether the stream supports writing.</summary>
         public override bool CanWrite
         {
             get { return (this.mode == CompressionMode.Compress ? true : false); }
         }
 
+        /// <summary>Gets a value indicating whether the stream supports seeking.</summary>
         public override bool CanSeek
         {
             get { return (false); }
         }
 
+        /// <summary>Gets a reference to the underlying stream.</summary>
         public Stream BaseStream
         {
             get { return (this.compressedStream); }
         }
 
         #region Not yet supported
+        /// <summary>Flushes the contents of the internal buffer of the current GZipStream object to the underlying stream.</summary>
         public override void Flush()
         {
-            throw new Exception("The method or operation is not implemented.");
+            throw new NotSupportedException("The method or operation is not implemented.");
         }
 
+        /// <summary>This property is not supported and always throws a NotSupportedException.</summary>
+        /// <param name="offset">The location in the stream.</param>
+        /// <param name="origin">One of the SeekOrigin values.</param>
+        /// <returns>A long value.</returns>
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotSupportedException();
         }
 
+        /// <summary>This property is not supported and always throws a NotSupportedException.</summary>
+        /// <param name="value">The length of the stream.</param>
         public override void SetLength(long value)
         {
             throw new NotSupportedException();
         }
 
-        public override void Write(byte[] buffer, int offset, int count)
+        /// <summary>This property is not supported and always throws a NotSupportedException.</summary>
+        /// <param name="array">The array used to store compressed bytes.</param>
+        /// <param name="offset">The location in the array to begin reading.</param>
+        /// <param name="count">The number of bytes compressed.</param>
+        public override void Write(byte[] array, int offset, int count)
         {
             throw new NotSupportedException("Not yet supported!");
         }
 
+        /// <summary>This property is not supported and always throws a NotSupportedException.</summary>
         public override long Length
         {
             get { throw new NotSupportedException(); }
         }
 
+        /// <summary>This property is not supported and always throws a NotSupportedException.</summary>
         public override long Position
         {
             get { throw new NotSupportedException(); }
