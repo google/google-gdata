fd990b87e33b3cc204a95941def3069c149b153f
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2007-11-27 16:52:27 +0000

    updated documentation build script

--- trunk/clients/cs/misc/pushdoc.cmd
+++ trunk/clients/cs/misc/pushdoc.cmd
@@ -3,10 +3,17 @@ set shfb_home=%ProgramFiles%\EWSoftware\Sandcastle Help File Builder
 set src_home=..\src\VS2003
 set out_dir=..\lib
 set doc_dir=..\docs
+set target_dir=..\docs\generated
+set temp_dir=..\docs\TempHTML
 
 rem build documentation
 "%shfb_home%\SandcastleBuilderConsole.exe" GDataProject.shfb
-move "%doc_dir%\HTMLDocumentation\Google.GData.Documentation.chm" "%doc_dir%"
-move "%doc_dir%\HTMLDocumentation\*.log" .
+move "%temp_dir%\Google.GData.Documentation.chm" "%doc_dir%\"
+move "%temp_dir%\TempHTML\*.log" .
 
-call createzip.cmd
+echo "%temp_dir%\*.*"
+echo "%target_dir%\*.*"
+move /y "%temp_dir%\*.*" "%target_dir%\"
+move /y "%temp_dir%\html\*.*" "%target_dir%\html\"
+
+
