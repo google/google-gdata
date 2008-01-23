038091c1200bab6d14abadeb664a678d8347a8c1
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2008-01-23 15:39:56 +0000

    On the road to 1.1.3.0

--- clients/cs/misc/pushdoc.cmd
+++ clients/cs/misc/pushdoc.cmd
@@ -5,7 +5,7 @@ set out_dir=..\lib
 set doc_dir=..\docs
 set target_dir=..\docs\generated
 set temp_dir=..\docs\TempHTML
-
+time /t
 rem build documentation
 "%shfb_home%\SandcastleBuilderConsole.exe" GDataProject.shfb
 move "%temp_dir%\Google.GData.Documentation.chm" "%doc_dir%\"
@@ -15,5 +15,6 @@ echo "%temp_dir%\*.*"
 echo "%target_dir%\*.*"
 move /y "%temp_dir%\*.*" "%target_dir%\"
 move /y "%temp_dir%\html\*.*" "%target_dir%\html\"
+time /t
 
 
