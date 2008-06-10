f230125f7bea2b026abfd402e9d7bc725a400ae9
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2008-06-10 11:17:06 +0000

    updated version for next release

--- trunk/clients/cs/version/AssemblyVersion.cs
+++ trunk/clients/cs/version/AssemblyVersion.cs
@@ -59,7 +59,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.9.9")]
+[assembly: AssemblyVersion("1.2.0.0")]
 
 
 
