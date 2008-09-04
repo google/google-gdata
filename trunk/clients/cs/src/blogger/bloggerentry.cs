e526cadd0c1907de5212c594407bb8fa3c4270fa
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2008-09-04 12:54:17 +0000

    working on implementing a real blogger service

---
+++ trunk/clients/cs/src/blogger/bloggerentry.cs
@@ -0,0 +1,42 @@
+/* Copyright (c) 2006 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Xml;
+using System.IO; 
+using System.Collections;
+using Google.GData.Client;
+using Google.GData.Extensions;
+
+namespace Google.GData.Blogger
+{
+
+    //////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// CalendarEntry API customization class for defining entries in a calendar feed.
+    /// </summary>
+    //////////////////////////////////////////////////////////////////////
+    public class BloggerEntry : AbstractEntry
+    {
+        
+
+        /// <summary>
+        /// Constructs a new CalenderEntry instance
+        /// </summary>
+        public BloggerEntry() : base()
+        {
+        }
+    }
+}
