0473a1339f94d70a59ad445669fb6393ce467ac2
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2008-12-12 15:22:12 +0000

    modified the script for the new structure

--- trunk/clients/cs/misc/pushdlls.cmd
+++ trunk/clients/cs/misc/pushdlls.cmd
@@ -1,48 +1,48 @@
 # build the whole release project, note this should RUN inside the VS dev prompt
 
 echo "Run this inside the Visual Studio Development Prompt"
-devenv /rebuild Release "..\src\VS2005\Google Data API SDK.sln"
-devenv /rebuild Debug "..\src\VS2005\Google Data API SDK.sln"
+devenv /rebuild Release "..\src\Google Data API SDK.sln"
+devenv /rebuild Debug "..\src\Google Data API SDK.sln"
 devenv /rebuild Release ..\src\VS2005.mobile\gdatamobile.sln
 
 # copy the DLLS
-xcopy /y ..\src\VS2005\gdata\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gbase\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gextension\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gspreadsheets\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gcalendar\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gcodesearch\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gapps\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gacl\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gphotos\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gdoclist\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\gcontacts\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\youtube\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\ghealth\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\VS2005\blogger\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\core\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gbase\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\extensions\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gspreadsheet\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gcalendar\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gcodesearch\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gapps\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gacl\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gphotos\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gdocuments\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gcontacts\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\youtube\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\ghealth\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\blogger\bin\Release\*.dll ..\lib\Release\*.*
 
 # copy the debug DLLS
-xcopy /y ..\src\VS2005\gdata\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gbase\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gextension\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gspreadsheets\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gcalendar\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gcodesearch\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gapps\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gacl\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gphotos\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gdoclist\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\gcontacts\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\youtube\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\ghealth\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\unittests\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\VS2005\blogger\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\core\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gbase\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\extensions\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gspreadsheet\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gcalendar\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gcodesearch\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gapps\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gacl\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gphotos\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gdocuments\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gcontacts\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\youtube\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\ghealth\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\unittests\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\blogger\bin\Debug\*.dll ..\lib\Debug\*.*
 
 
 # copy asp dlls for the youtube sample
-xcopy /y ..\src\VS2005\gdata\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
-xcopy /y ..\src\VS2005\gdata\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
-xcopy /y ..\src\VS2005\youtube\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
+xcopy /y ..\src\gdata\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
+xcopy /y ..\src\gdata\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
+xcopy /y ..\src\youtube\bin\asp\*.dll ..\samples\YouTubeSample\bin\*.*
  
 
 
@@ -82,25 +82,25 @@ xcopy /y ..\samples\YouTubeNotifier\bin\Release\*.exe ..\lib\Release\*.*
 
 
 # copy the xml doc files
-xcopy /y ..\src\VS2005\gdata\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gbase\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gextension\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gspreadsheets\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gcalendar\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gcodesearch\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gapps\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gacl\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gphotos\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gdoclist\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\gcontacts\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\youtube\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\ghealth\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\VS2005\blogger\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gdata\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gbase\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gextension\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gspreadsheets\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gcalendar\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gcodesearch\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gapps\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gacl\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gphotos\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gdoclist\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\gcontacts\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\youtube\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\ghealth\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\blogger\bin\*.xml ..\docs\*.*
 
 #copy the Merge module files
 
-xcopy /y ..\src\VS2005\CoreMergeModule\Release\*.msm ..\lib\MergeModules\*.*
-xcopy /y ..\src\VS2005\YouTubeMergeModule\Release\*.msm ..\lib\MergeModules\*.*
+xcopy /y ..\src\CoreMergeModule\Release\*.msm ..\lib\MergeModules\*.*
+xcopy /y ..\src\YouTubeMergeModule\Release\*.msm ..\lib\MergeModules\*.*
 
 
 rem run ILMerge on PhotoBrowser.exe
