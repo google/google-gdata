de13ad20b246fa7da600065ab34edde37d1d4d16
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2009-02-26 14:57:02 +0000

    Pushing 1.4.0.1 build

--- trunk/clients/cs/samples/YouTubeNotifier/Properties/AssemblyInfo.cs
+++ trunk/clients/cs/samples/YouTubeNotifier/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YouTubeNofifier sample Application")]
+[assembly: AssemblyTitle("Notifier for YouTube sample Application")]
 [assembly: AssemblyConfiguration("")]
 
 
