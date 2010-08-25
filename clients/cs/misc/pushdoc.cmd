1302428d4cc6960aa3f429c0af6775d14221737e
fmantek <fmantek@b7824a5d-8719-0410-90f2-77ed95a13428>
2010-08-25 10:49:30 +0000

    Fixed the batch job

--- clients/cs/misc/pushdoc.cmd
+++ clients/cs/misc/pushdoc.cmd
@@ -1,22 +1,23 @@
-@echo off
-set shfb_home=%ProgramFiles%\EWSoftware\Sandcastle Help File Builder
+#@echo off
 set out_dir=..\lib
 set doc_dir=..\docs
 set target_dir=..\..\..\..\docs
-set temp_dir=d:\tempHTML
+set temp_dir=d:\gdata.doc
 time /t
 rem build documentation
-"%shfb_home%\SandcastleBuilderConsole.exe" GDataProject.shfb
+msbuild.exe gdatadocumentation.shfbproj
 move "%temp_dir%\Google.GData.Documentation.chm" "%doc_dir%\"
 move "%temp_dir%\*.log" .
 
 echo "%temp_dir%\*.*"
 echo "%target_dir%\*.*"
 
-move /y "%temp_dir%\*.*" "%target_dir%\"
-move /y "%temp_dir%\html\*.*" "%target_dir%\html\"
-move /y "%target_dir%\Index.html" "%target_dir%\orgindex.html"
-move /y "%temp_dir%\AdditionalContent\*.*" "target_dir%\AdditionalContent\"
+mkdir "%target_dir%\html"
+
+copy /y "%temp_dir%\*.*" "%target_dir%\*.*"
+copy /y "%temp_dir%\html\*.*" "%target_dir%\html\"
+copy /y "%target_dir%\Index.html" "%target_dir%\orgindex.html"
+copy /y "%temp_dir%\Additonal Content\*.*" "%target_dir%\AdditionalContent\"
 time /t
 
 
