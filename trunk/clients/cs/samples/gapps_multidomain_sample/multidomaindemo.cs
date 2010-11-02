220f68d905cc291bfbaf5a5a1333d05c2c97c18b
ccherubino@google.com <ccherubino@google.com@b7824a5d-8719-0410-90f2-77ed95a13428>
2010-11-02 15:10:58 +0000

    Added Google Apps multidomain service and sample

---
+++ trunk/clients/cs/samples/gapps_multidomain_sample/multidomaindemo.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.GData.Extensions.Apps;
+using Google.GData.Apps;
+
+
+namespace GoogleAppsConsoleApplication
+{
+    class AppsMultiDomainDemo
+    {
+        private static string primaryDomain;
+        private static string secondaryDomain;
+        private static string adminEmail;
+        private static string adminPassword;
+        private static string testUserEmail;
+
+        /// <summary>
+        /// This console application demonstrates all the Google Apps
+        /// MultiDomain Management APIs. 
+        /// </summary>
+        /// <param name="args">Command-line arguments: args[0] is
+        /// the primary domain, args[1] is the secondary domain,
+        /// args[2] is the admin email address, args[3] is the admin 
+        /// password and args[4] is test user(email address)
+        /// 
+        /// Example: AppsMultiDomainDemo example.com alias.com admin@example.com my_password test_user_email</param>
+        public static void Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Syntax: AppsMultiDomainDemo <primary_domain> <secondary_domain> <admin_email> <admin_password> <testUserEmail>");
+            }
+            else
+            {
+                primaryDomain = args[0];
+                secondaryDomain = args[1];
+                adminEmail = args[2];
+                adminPassword = args[3];
+                testUserEmail = args[4];
+
+                MultiDomainManagementService service = new MultiDomainManagementService(primaryDomain, "multidomainapis-apps-demo");
+                service.setUserCredentials(adminEmail, adminPassword);
+
+                RunSample(service);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="service"></param>
+        private static void RunSample(MultiDomainManagementService service)
+        {
+            const String testUserPassword = "password";
+            const String testUserFirstName = "Liz";
+            const String testUserLastName = "Smith";
+            const bool testUserIsAdmin = true;
+            const String testUserNewFirstName = "Elizabeth";
+            String testUserNewEmail = "liz@" + secondaryDomain;
+            String testUserAliasEmail = "helpdesk@" + secondaryDomain;
+
+            try
+            {
+                // Create a new Domain User
+                Console.WriteLine("\n-----------Creating domain user-----------");
+                AppsExtendedEntry entry = service.CreateDomainUser(primaryDomain, testUserEmail, testUserPassword,
+                                                 testUserFirstName, testUserLastName, testUserIsAdmin);
+                Console.WriteLine("Created: " +
+                                  entry.getPropertyValueByName(AppsMultiDomainNameTable.UserEmail));
+
+                // Update Domain User and list all properties
+                Console.WriteLine("\n-----------Updating domain user----");
+                IDictionary<MultiDomainManagementService.MultiDomainUserProperty, string> updates =
+                    new Dictionary<MultiDomainManagementService.MultiDomainUserProperty, string>();
+                updates[MultiDomainManagementService.MultiDomainUserProperty.FirstName] = testUserNewFirstName;
+                entry = service.UpdateDomainUser(primaryDomain, testUserEmail, updates);
+                foreach (PropertyElement element in entry.Properties)
+                {
+                    Console.WriteLine(String.Format("{0} - {1}", element.Name, element.Value));
+                }
+
+                // Create a new Domain User to be renamed
+                Console.WriteLine("\n-----------Creating domain user to be renamed-----------");
+                String tempEmail = "TOBERENAMED@" + primaryDomain;
+                entry = service.CreateDomainUser(primaryDomain, tempEmail, testUserPassword,
+                                                 testUserFirstName, testUserLastName, testUserIsAdmin);
+                Console.WriteLine("Created: " +
+                                  entry.getPropertyValueByName(AppsMultiDomainNameTable.UserEmail));
+
+                // Rename Domain User
+                Console.WriteLine("\n-----------Renaming domain user---------------------");
+                entry = service.RenameDomainUser(primaryDomain, tempEmail, testUserNewEmail);
+                Console.WriteLine("Renamed domain user: " +
+                                  entry.getPropertyValueByName(AppsMultiDomainNameTable.NewEmail));
+
+                // Retrieve Domain User
+                Console.WriteLine("\n-----------Retrieving domain user----");
+                entry = service.RetrieveDomainUser(primaryDomain, testUserEmail);
+                String firstName =
+                    entry.getPropertyValueByName(AppsMultiDomainNameTable.FirstName);
+                Console.WriteLine("FirstName: " + firstName);
+
+                // Retrieve all domain users unit and list the emails
+                Console.WriteLine("\n-----------Retrieving all domain users----");
+                AppsExtendedFeed feed = service.RetrieveAllDomainUsers(primaryDomain);
+                foreach (AppsExtendedEntry unit in feed.Entries)
+                {
+                    Console.WriteLine(
+                        unit.getPropertyValueByName(AppsMultiDomainNameTable.UserEmail));
+                }
+
+                // Create a new User Alias
+                Console.WriteLine("\n-----------Creating user alias-----------");
+                entry = service.CreateDomainUserAlias(primaryDomain, testUserEmail, testUserAliasEmail);
+                Console.WriteLine("Created Alias: " +
+                                  entry.getPropertyValueByName(AppsMultiDomainNameTable.AliasEmail));
+
+                // Retrieve User Alias
+                entry = service.RetrieveDomainUserAlias(primaryDomain, testUserAliasEmail);
+                String userEmail =
+                    entry.getPropertyValueByName(AppsMultiDomainNameTable.UserEmail);
+                Console.WriteLine("UserEmail: " + userEmail);
+
+                // Retrieve all user aliases for the domain
+                Console.WriteLine("\n-----------Retrieving all user aliases----");
+                feed = service.RetrieveAllDomainUserAlias(primaryDomain);
+                foreach (AppsExtendedEntry unit in feed.Entries)
+                {
+                    Console.WriteLine(
+                        unit.getPropertyValueByName(AppsMultiDomainNameTable.UserEmail));
+                }
+
+                // Retrieve all aliases for an user
+                Console.WriteLine("\n-----------Retrieving all aliases for user----");
+                feed = service.RetrieveAllDomainUserAliasForUser(primaryDomain, testUserEmail);
+                foreach (AppsExtendedEntry unit in feed.Entries)
+                {
+                    Console.WriteLine(
+                        unit.getPropertyValueByName(AppsMultiDomainNameTable.AliasEmail));
+                }
+
+                // Delete User Alias
+                Console.WriteLine("\n-----------Deleting alias----");
+                service.DeleteDomainUserAlias(primaryDomain, testUserAliasEmail);
+
+                // Delete User
+                Console.WriteLine("\n-----------Deleting user----");
+                service.DeleteDomainUser(primaryDomain, testUserEmail);
+            }
+            catch (AppsException a)
+            {
+                Console.WriteLine("A Google Apps error occurred.");
+                Console.WriteLine();
+                Console.WriteLine("Error code: {0}", a.ErrorCode);
+                Console.WriteLine("Invalid input: {0}", a.InvalidInput);
+                Console.WriteLine("Reason: {0}", a.Reason);
+            }
+        }
+    }
+}
---
+++ trunk/clients/cs/src/gapps/multidomainmanagementservice.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Google.GData.Extensions.Apps;
+
+namespace Google.GData.Apps
+{
+	/// <summary>
+	/// Service class for managing multiple domains within Google Apps. 
+	/// </summary>
+    public class MultiDomainManagementService : AppsPropertyService
+    {
+        public enum MultiDomainUserProperty
+        {
+            AliasEmail, FirstName, IpWhitelisted, IsAdmin, IsChangePasswordAtNextLogin, IsSuspended, LastName, NewEmail, Password, UserEmail
+        }
+
+        public MultiDomainManagementService(String domain, String applicationName)
+            : base(domain, applicationName)
+        {
+        }
+
+		/// <summary>
+		/// Creates a new user for the specified domain.
+		/// </summary>
+        /// <param name="domain">The domain to use to create the user</param>
+        /// <param name="userEmail">The user's email address</param>
+        /// <param name="password">The user's password</param>
+        /// <param name="firstName">The user's first name</param>
+		/// <param name="lastName">The user's last name</param>
+        /// <param name="isAdmin">Whether the user is an administrator for the domain</param>
+        /// <returns>The created user</returns>
+        public AppsExtendedEntry CreateDomainUser(String domain, String userEmail, String password,
+            String firstName, String lastName, bool isAdmin)
+        {
+            Uri userUri = new Uri(String.Format("{0}/{1}",
+                AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri, domain));
+
+            AppsExtendedEntry entry = new AppsExtendedEntry();
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.UserEmail, userEmail));
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.Password, password));
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.FirstName, firstName));
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.LastName, lastName));
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.IsAdmin, isAdmin.ToString()));
+            return Insert(userUri, entry);
+        }
+
+        /// <summary>
+        /// Updates the given user
+        /// </summary>
+        /// <param name="domain">The user's domain</param>
+        /// <param name="userEmail">The user's email address</param>
+        /// <param name="attributes">The set of attributes to update</param>
+		/// <returns>The updated user</returns>
+        public AppsExtendedEntry UpdateDomainUser(String domain, String userEmail, IDictionary<MultiDomainUserProperty, String> attributes)
+        {
+            AppsExtendedEntry entry = new AppsExtendedEntry();
+            String uri = String.Format("{0}/{1}/{2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri, domain, userEmail);
+            entry.EditUri = new Uri(uri);
+
+            foreach (KeyValuePair<MultiDomainUserProperty, String> mapEntry in attributes)
+            {
+                String value = mapEntry.Value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    continue;
+                }
+
+                switch (mapEntry.Key)
+                {
+                    case MultiDomainUserProperty.FirstName:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.FirstName, value));
+                        break;
+                    case MultiDomainUserProperty.IpWhitelisted:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.IpWhitelisted, value));
+                        break;
+                    case MultiDomainUserProperty.IsAdmin:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.IsAdmin, value));
+                        break;
+                    case MultiDomainUserProperty.IsChangePasswordAtNextLogin:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.IsChangePasswordAtNextLogin, value));
+                        break;
+                    case MultiDomainUserProperty.IsSuspended:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.IsSuspended, value));
+                        break;
+                    case MultiDomainUserProperty.LastName:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.LastName, value));
+                        break;
+                    case MultiDomainUserProperty.NewEmail:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.NewEmail, value));
+                        break;
+                    case MultiDomainUserProperty.Password:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.Password, value));
+                        break;
+                    case MultiDomainUserProperty.UserEmail:
+                        entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.UserEmail, value));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Update(entry);
+        }
+
+        /// <summary>
+        /// Renames an existing user
+        /// </summary>
+		/// <param name="domain">The user's domain</param>
+		/// <param name="userEmail">Current user's email address</param>
+		/// <param name="newEmail">New user's email address</param>
+        /// <returns>The renamed user</returns>
+        public AppsExtendedEntry RenameDomainUser(String domain, String userEmail, String newEmail)
+        {
+            AppsExtendedEntry entry = new AppsExtendedEntry();
+            String uri = String.Format("{0}/{1}/{2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri, domain, userEmail);
+            entry.EditUri = new Uri(uri);
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.NewEmail, newEmail));
+            return Update(entry);
+        }
+
+        /// <summary>
+        /// Retrieves an user from the domain
+        /// </summary>
+		/// <param name="domain">The user's domain</param>
+		/// <param name="userEmail">The user's email address</param>
+        /// <returns>The user identified by the given email address</returns>
+        public AppsExtendedEntry RetrieveDomainUser(String domain, String userEmail)
+        {
+            String uri = String.Format("{0}/{1}/{2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri,
+                                       domain, userEmail);
+
+            return Get(uri) as AppsExtendedEntry;
+        }
+
+        /// <summary>
+        /// Retrieves all users from the domain
+        /// </summary>
+        /// <param name="domain">The users' domain</param>
+        /// <returns>Feed containing all domain users</returns>
+        public AppsExtendedFeed RetrieveAllDomainUsers(String domain)
+        {
+            String uri = String.Format("{0}/{1}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri,
+                                       domain);
+
+            return QueryExtendedFeed(new Uri(uri), true);
+        }
+
+        /// <summary>
+        /// Deletes an user from the domain
+        /// </summary>
+		/// <param name="domain">The user's domain</param>
+		/// <param name="userEmail">The user's email address</param>
+        public void DeleteDomainUser(String domain, String userEmail)
+        {
+            String uri = String.Format("{0}/{1}/{2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainUserBaseFeedUri,
+                                       domain,
+                                       userEmail);
+            Delete(new Uri(uri));
+        }
+
+        /// <summary>
+        /// Creates an alias for an user in the domain
+        /// </summary>
+		/// <param name="domain">The user's domain</param>
+		/// <param name="userEmail">The user's email address</param>
+        /// <param name="aliasEmail">The alias to be added to the user</param>
+        /// <returns>The created alias</returns>
+        public AppsExtendedEntry CreateDomainUserAlias(String domain, String userEmail, String aliasEmail)
+        {
+            Uri userUri = new Uri(String.Format("{0}/{1}",
+                AppsMultiDomainNameTable.AppsMultiDomainAliasBaseFeedUri, domain));
+
+            AppsExtendedEntry entry = new AppsExtendedEntry();
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.UserEmail, userEmail));
+            entry.Properties.Add(new PropertyElement(AppsMultiDomainNameTable.AliasEmail, aliasEmail));
+            return Insert(userUri, entry);
+        }
+
+        /// <summary>
+        /// Retrieves an alias for the user in the domain
+        /// </summary>
+		/// <param name="domain">The user alias's domain or the domain in the user's primary email address</param>
+		/// <param name="aliasEmail">The alias's email address</param>
+        /// <returns>The alias identified by the email address</returns>
+        public AppsExtendedEntry RetrieveDomainUserAlias(String domain, String aliasEmail)
+        {
+            String uri = String.Format("{0}/{1}/{2}",
+                                    AppsMultiDomainNameTable.AppsMultiDomainAliasBaseFeedUri,
+                                    domain, aliasEmail);
+
+            return Get(uri) as AppsExtendedEntry;
+        }
+
+        /// <summary>
+		/// Retrieves all aliases from the domain
+        /// </summary>
+		/// <param name="domain">The aliases' domain or a user's primary email address domain</param>
+        /// <returns>Feed containing all aliases for the domain</returns>
+        public AppsExtendedFeed RetrieveAllDomainUserAlias(String domain)
+        {
+            String uri = String.Format("{0}/{1}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainAliasBaseFeedUri,
+                                       domain);
+
+            return QueryExtendedFeed(new Uri(uri), true);
+        }
+
+        /// <summary>
+		/// Retrieves all aliases from the domain for an user
+        /// </summary>
+		/// <param name="domain">The alias's domain or the user's primary email address domain</param>
+		/// <param name="userEmail">The user's email address</param>
+        /// <returns>Feed containing all aliases for the domain user</returns>
+        public AppsExtendedFeed RetrieveAllDomainUserAliasForUser(String domain, String userEmail)
+        {
+            String uri = String.Format("{0}/{1}?userEmail={2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainAliasBaseFeedUri,
+                                       domain,
+                                       userEmail);
+
+            return QueryExtendedFeed(new Uri(uri), true);
+        }
+
+        /// <summary>
+        /// Deletes an alias from a domain user
+        /// </summary>
+		/// <param name="domain">The alias's domain or the user's primary email address domain</param>
+		/// <param name="userEmail">The user's email address</param>
+        public void DeleteDomainUserAlias(String domain, String userEmail)
+        {
+            String uri = String.Format("{0}/{1}/{2}",
+                                       AppsMultiDomainNameTable.AppsMultiDomainAliasBaseFeedUri,
+                                       domain,
+                                       userEmail);
+            Delete(new Uri(uri));
+        }
+    }
+}
+﻿
