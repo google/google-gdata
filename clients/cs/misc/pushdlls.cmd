6a4a0f43be1872af8309485b72fff98a50520875
Claudio Cherubino <claudiocherubino@gmail.com>
2011-09-01 23:08:15 +0000

    Preparing solution for 1.9.0.0 release

--- clients/cs/misc/pushdlls.cmd
+++ clients/cs/misc/pushdlls.cmd
@@ -13,40 +13,40 @@ devenv /rebuild Release "..\src\youtube\YouTube SDK\YouTube SDK.sln"
 if errorlevel 1 goto buildError
 
 # copy the DLLS
+xcopy /y ..\src\analytics\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\blogger\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\contentforshopping\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\core\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\documents3\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\extensions\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\gspreadsheet\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gacl\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gapps\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\gcalendar\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\gcodesearch\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\gapps\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\gacl\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\gphotos\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\documents3\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\gcontacts\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\youtube\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\ghealth\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\blogger\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\analytics\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gphotos\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\gspreadsheet\bin\Release\*.dll ..\lib\Release\*.*
 xcopy /y ..\src\WebmasterTools\bin\Release\*.dll ..\lib\Release\*.*
-xcopy /y ..\src\contentforshopping\bin\Release\*.dll ..\lib\Release\*.*
+xcopy /y ..\src\youtube\bin\Release\*.dll ..\lib\Release\*.*
 
 # copy the debug DLLS
+xcopy /y ..\src\analytics\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\blogger\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\contentforshopping\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\core\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\documents3\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\extensions\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\gspreadsheet\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gacl\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gapps\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\gcalendar\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\gcodesearch\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\gapps\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\gacl\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\gphotos\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\documents3\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\gcontacts\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\youtube\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\ghealth\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\blogger\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\analytics\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gphotos\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\gspreadsheet\bin\Debug\*.dll ..\lib\Debug\*.*
 xcopy /y ..\src\WebmasterTools\bin\Debug\*.dll ..\lib\Debug\*.*
-xcopy /y ..\src\contentforshopping\bin\Debug\*.dll ..\lib\Debug\*.*
+xcopy /y ..\src\youtube\bin\Debug\*.dll ..\lib\Debug\*.*
 
 # copy the YouTube setup
 xcopy /y "..\src\youtube\YouTube SDK\YouTube SDK\Release\YouTube SDK.msi" ..\lib\Setup\*.*
@@ -79,25 +79,28 @@ xcopy /y ..\samples\Analytics_AccountFeed_Sample\bin\Release\*.exe ..\lib\Releas
 xcopy /y ..\samples\Analytics_DataFeed_Sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\appsforyourdomain\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\Blogger\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\samples\BookThemAll\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\Calendar\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\CodeSearch\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\samples\contentforshopping_sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\DocListExporter\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\DocListUploader\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\ExecRequest\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\samples\gapps_audit_sample\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\samples\gapps_calendar_resource_sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\gapps_google_mail_settings_sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\gapps_multidomain_sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\gapps_orgmanagement_sample\bin\Release\*.exe ..\lib\Release\*.*
-xcopy /y ..\samples\gapps_calendar_resource_sample\bin\Release\*.exe ..\lib\Release\*.*
-xcopy /y ..\samples\gapps_audit_sample\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\health\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\PhotoBrowser\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\spreadsheets\bin\Release\*.exe ..\lib\Release\*.*
+xcopy /y ..\samples\unshare_profiles\bin\Release\*.exe ..\lib\Release\*.*
 xcopy /y ..\samples\YouTubeNotifier\bin\Release\*.exe ..\lib\Release\*.*
-xcopy /y ..\samples\contentforshopping\bin\Release\*.exe ..\lib\Release\*.*
 
 # copy the xml doc files
 xcopy /y ..\src\analytics\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\blogger\bin\*.xml ..\docs\*.*
+xcopy /y ..\src\contentforshopping\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\core\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\documents3\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\extensions\bin\*.xml ..\docs\*.*
@@ -111,7 +114,6 @@ xcopy /y ..\src\gphotos\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\gspreadsheets\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\webmastertools\bin\*.xml ..\docs\*.*
 xcopy /y ..\src\youtube\bin\*.xml ..\docs\*.*
-xcopy /y ..\src\contentforshopping\bin\*.xml ..\docs\*.*
 
 cd ..\lib\release\
 
