7d8679ee1ff1536db5c0335695a56cc56caf186d
ccherubino@google.com <ccherubino@google.com@b7824a5d-8719-0410-90f2-77ed95a13428>
2011-10-17 23:51:28 +0000

    Updated DocListUploader sample application to use HTTPS endpoints

--- trunk/clients/cs/samples/DocListUploader/Properties/AssemblyInfo.cs
+++ trunk/clients/cs/samples/DocListUploader/Properties/AssemblyInfo.cs
@@ -6,28 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DocListUploader")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Google")]
-[assembly: AssemblyProduct("DocListUploader")]
-[assembly: AssemblyCopyright("Copyright © Google 2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cc56bada-955e-4d91-9bf8-3e71c238833b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
