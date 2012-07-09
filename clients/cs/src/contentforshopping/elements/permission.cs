6722e53b691fc3bb61df34555da3744e9f68750b
Claudio Cherubino <claudiocherubino@gmail.com>
2012-07-09 18:22:36 +0000

    Adding support for the User Management Feed to the Content for Shopping API service

---
+++ clients/cs/src/contentforshopping/elements/permission.cs
@@ -0,0 +1,61 @@
+﻿/* Copyright (c) 2006 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
+using Google.GData.Extensions;
+
+namespace Google.GData.ContentForShopping.Elements
+{
+    public class Permission : SimpleElement
+    {
+        /// <summary>
+        /// default constructor for sc:permission
+        /// </summary>
+        public Permission()
+            : base(ContentForShoppingNameTable.Permission,
+               ContentForShoppingNameTable.scDataPrefix,
+               ContentForShoppingNameTable.BaseNamespace)
+        {
+        }
+
+        /// <summary>
+        /// constructor that takes status for sc:permission
+        /// </summary>
+        /// <param name="scope">The permission's scope.</param>
+        /// <param name="value">The permission's value.</param>
+        public Permission(string scope, string value)
+            : base(ContentForShoppingNameTable.Permission,
+               ContentForShoppingNameTable.scDataPrefix,
+               ContentForShoppingNameTable.BaseNamespace)
+        {
+            this.Scope = scope;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Scope property accessor
+        /// </summary>
+        public string Scope
+        {
+            get
+            {
+                return Convert.ToString(Attributes[ContentForShoppingNameTable.Scope]);
+            }
+            set
+            {
+                Attributes[ContentForShoppingNameTable.Scope] = value;
+            }
+        }
+    }
+}
---
+++ clients/cs/src/contentforshopping/usersentry.cs
@@ -0,0 +1,71 @@
+﻿/* Copyright (c) 2006 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using Google.GData.Client;
+using Google.GData.Extensions;
+using System.Globalization;
+using Google.GData.ContentForShopping.Elements;
+using Google.GData.Extensions.AppControl;
+
+namespace Google.GData.ContentForShopping {
+    /// <summary>
+    /// Feed API customization class for defining Product feed.
+    /// </summary>
+    public class UsersEntry : AbstractEntry {
+        /// <summary>
+        /// Permission collection
+        /// </summary>
+        private ExtensionCollection<Permission> permissions;
+
+        public UsersEntry()
+            : base() {
+            this.AddExtension(new Admin());
+            this.AddExtension(new Permission());
+        }
+
+        /// <summary>
+        /// Admin
+        /// </summary>
+        public bool Admin {
+            get {
+                bool value;
+                if (!bool.TryParse(GetStringValue<Admin>(ContentForShoppingNameTable.Admin,
+                    ContentForShoppingNameTable.BaseNamespace), out value)) {
+                    value = false;
+                }
+
+                return value;
+            }
+            set {
+                SetStringValue<Admin>(value.ToString(),
+                    ContentForShoppingNameTable.Admin,
+                    ContentForShoppingNameTable.BaseNamespace);
+            }
+        }
+
+        /// <summary>
+        /// Permission collection.
+        /// </summary>
+        public ExtensionCollection<Permission> Permissions {
+            get {
+                if (this.permissions == null) {
+                    this.permissions = new ExtensionCollection<Permission>(this);
+                }
+                return this.permissions;
+            }
+        }
+    }
+}
